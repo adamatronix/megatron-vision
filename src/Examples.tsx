f68c10f14b5de3ec6f433d8bf8b8b219f7ace6bf
adamatronix <you@example.com>
2022-09-24 16:28:59 +0800

    r1.0.4 Add power preference to make it work better on Safari and add stat option to check performance

--- src/Examples.tsx
+++ src/Examples.tsx
@@ -23,7 +23,7 @@ const Example = () => {
   const megatronRef = useRef<MegatronVision>();
 
   useEffect(() => {
-    megatronRef.current = new MegatronVision(elRef.current,{ autoPlay: false, muted: false, src: videoUrl, endedCallback: () => { console.log('video ended')} });
+    megatronRef.current = new MegatronVision(elRef.current,{ autoPlay: false, muted: false, stats: true, src: videoUrl, endedCallback: () => { console.log('video ended')} });
 
     return () => {
       megatronRef.current.destroy();
